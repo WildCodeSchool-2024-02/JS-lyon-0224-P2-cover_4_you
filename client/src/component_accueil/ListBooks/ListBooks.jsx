6811a3c54546bcde200126933d95345d8aa8ad4a
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-04-11 16:05:28 +0200

    new push cause github fucked up

--- client/src/component_accueil/ListBooks/ListBooks.jsx
+++ client/src/component_accueil/ListBooks/ListBooks.jsx
@@ -1,14 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import styles from "./ListBooks.module.css";
-
+import BookCard from './BookCard'
 
 function Books() {
 
-  const [bookTitle, setBookTitle] = useState(null)
-  const [image, setImage] = useState(null)
-  const [isbnCode, setIsbnCode] = useState(null)
-
+  const [bookList, setBookList] = useState([])
 
   useEffect(() => {
     let ignore = false;
@@ -17,19 +13,8 @@ function Books() {
         try {
           await axios.get("https://openlibrary.org/search.json?q=the+lord+of+the+rings")
             .then((response) => {
-              setBookTitle(response.data.docs[0])
-              setIsbnCode(response.data.docs[0].isbn[0])
+              setBookList([response.data.docs[0]])
             });
-
-          // ici
-          await axios.get("https://covers.openlibrary.org/b/ISBN/9780739408254-M.jpg")
-            .then((response) => {
-              console.log(response);
-              setImage(response)
-            });
-          // ici
-          
-
         }
         catch (error) {
           console.error('Error fetching data:', error);
@@ -42,21 +27,10 @@ function Books() {
     }
   }, []);
 
-  function canDisplay(element) {
-    return bookTitle !== null ? bookTitle[element] : 'Loading...';
-  }
+
   return (
-
-    <div className={styles.globalBook}>
-      <p className={styles.titleBook}>{canDisplay('title')} </p>
-      <p className={styles.author}>{canDisplay('author_name')} </p>
-
-      <img src='https://covers.openlibrary.org/b/ISBN/9780739408254-M.jpg' alt='' />
-
-      <p>{canDisplay('first_publish_year')}</p>
-      <button type="button">✏️</button>
-    </div>
-  );
+    bookList.map(book => (<BookCard key={book.id_librarything[0]} book={book} />)
+  ));
 }
 
 export default Books
