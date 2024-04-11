e02482fd6f849274173fff1a5eb6f0d976ede7da
Loic legoff <loic95280@gmail.com>
2024-04-11 12:35:10 +0200

    add ISBN image

--- client/src/component_accueil/ListBooks/ListBooks.jsx
+++ client/src/component_accueil/ListBooks/ListBooks.jsx
@@ -6,19 +6,34 @@ import styles from "./ListBooks.module.css";
 function Books() {
 
   const [bookTitle, setBookTitle] = useState(null)
+  const [image, setImage] = useState(null)
+  const [isbnCode, setIsbnCode] = useState(null)
+
 
   useEffect(() => {
     let ignore = false;
     async function startFetching() {
       if (!ignore) {
-      try{
-      await axios.get("https://openlibrary.org/search.json?q=the+lord+of+the+rings")
-      .then((response) => {
-        setBookTitle(response.data.docs[0])});
+        try {
+          await axios.get("https://openlibrary.org/search.json?q=the+lord+of+the+rings")
+            .then((response) => {
+              setBookTitle(response.data.docs[0])
+              setIsbnCode(response.data.docs[0].isbn[0])
+            });
+
+
+          await axios.get("https://covers.openlibrary.org/b/ISBN/9780739408254-M.jpg")
+          .then((response) => {
+            console.log(response);
+            setImage(response)
+          });
+
+
+        }
+        catch (error) {
+          console.error('Error fetching data:', error);
+        }
       }
-      catch (error) {
-        console.error('Error fetching data:', error);
-      }}
     }
     startFetching();
     return () => {
@@ -26,16 +41,17 @@ function Books() {
     }
   }, []);
 
-  function canDisplay(element){
-    return bookTitle !== null? bookTitle[element] : 'Loading...';
+  function canDisplay(element) {
+    return bookTitle !== null ? bookTitle[element] : 'Loading...';
   }
-
   return (
 
     <div className={styles.globalBook}>
-      <p className={styles.titleBook}>{canDisplay('title')} </p> 
-      <p className='author'>{canDisplay('author_name')} </p>
-      <img alt='' src='https://media.discordapp.net/attachments/1224365307125170207/1227190331095908352/Patient-0-book.png?ex=6627810f&is=66150c0f&hm=3774d4693929db84eb632142fa8f15ae4fd2e789bd196099815d490cf8d193be&=&format=webp&quality=lossless&width=512&height=662' width="150px"/>
+      <p className={styles.titleBook}>{canDisplay('title')} </p>
+      <p className={styles.author}>{canDisplay('author_name')} </p>
+
+      <img src='https://covers.openlibrary.org/b/ISBN/9780739408254-M.jpg' alt='' />
+
       <p>{canDisplay('first_publish_year')}</p>
       <button type="button">✏️</button>
     </div>
