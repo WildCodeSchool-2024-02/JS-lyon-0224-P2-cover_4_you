62facb1e285f729d461c4d1c2e2c15addd9d489f
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-04-11 11:29:34 +0200

    moved useEffect() to ListBooks, it displays author + title + year

--- client/src/component_accueil/ListBooks.jsx
+++ client/src/component_accueil/ListBooks.jsx
@@ -1,29 +1,43 @@
-import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function Books() {
-  const [book, setBook] = useState("Title Book");
 
-  const getBook = () => {
-    axios
-      .get('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
+  const [bookTitle, setBookTitle] = useState(null)
+
+  useEffect(() => {
+    let ignore = false;
+    async function startFetching() {
+      if (!ignore) {
+      try{
+      await axios.get("https://openlibrary.org/search.json?q=the+lord+of+the+rings")
       .then((response) => {
-        setBook(response.data.docs[9].title);
-      })
-  };
+        setBookTitle(response.data.docs[0])});
+      }
+      catch (error) {
+        console.error('Error fetching data:', error);
+      }}
+    }
+    startFetching();
+    return () => {
+      ignore = true;
+    }
+  }, []);
+
+  function canDisplay(element){
+    return bookTitle !== null? bookTitle[element] : 'Loading...';
+  }
 
   return (
 
     <div className="globalBook">
-      <p className='titleBook'>{book}</p>
-      <img src='https://media.discordapp.net/attachments/1224365307125170207/1227190331095908352/Patient-0-book.png?ex=6627810f&is=66150c0f&hm=3774d4693929db84eb632142fa8f15ae4fd2e789bd196099815d490cf8d193be&=&format=webp&quality=lossless&width=512&height=662' width={"150px"}></img>
-      <button type="button" onClick={getBook}>Click</button>
+      <p className='titleBook'>{canDisplay('title')} </p> 
+      <p className='author'>{canDisplay('author_name')} </p>
+      <img alt='' src='https://media.discordapp.net/attachments/1224365307125170207/1227190331095908352/Patient-0-book.png?ex=6627810f&is=66150c0f&hm=3774d4693929db84eb632142fa8f15ae4fd2e789bd196099815d490cf8d193be&=&format=webp&quality=lossless&width=512&height=662' width="150px"/>
+      <p>{canDisplay('first_publish_year')}</p>
     </div>
 
   );
 }
 
 export default Books
-
-
-
