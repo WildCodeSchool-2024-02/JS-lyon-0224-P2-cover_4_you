0373bb50af78cea4940b6a7bf2b2836fb710d1a3
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-04-16 15:49:44 +0200

    changed path because ild one was obsolete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router-dom";
-import Nav from "../component_accueil/Nav/Nav";
-import Books from "../component_accueil/ListBooks/ListBooks";
+import Nav from "../component_home/Nav/Nav";
+import Books from "../component_home/ListBooks/ListBooks";
 // import Footer from "../component_accueil/Footer/Footer";
 
 export default function Home() {
