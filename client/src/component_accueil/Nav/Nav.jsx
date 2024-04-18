fc8f8f0298c9bcf6260b3d04caa40cbf08aae671
laura baratier <laura.baratierpro@gmail.com>
2024-04-18 12:09:13 +0200

    Navbar with burger menu finished

--- client/src/component_accueil/Nav/Nav.jsx
+++ client/src/component_accueil/Nav/Nav.jsx
@@ -1,8 +1,6 @@
-// import PropTypes from "prop-types";
-// import { useClickAway } from "react-use";
-// import { useRef } from "react";
 import { useState } from "react";
 import styles from "./Nav.module.css";
+// import styles from "../../App.module.css";
 
 function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,44 +12,41 @@ function Nav() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.nav_container}>
-      <img
-              className={styles.logo_Cover4You}
-              src="src/assets/images/logo_accueil.png"
-              alt="logo Cover4You"
-            />
+        <div className={styles.logo_Cover4You}/>
 
-      <button
-              type="button"
-              id="button_navbar"
-              onClick={toggleMenu}
-              className={
-                isMenuOpen === true
-                  ? styles.burger_nav_open
-                  : styles.cross_nav_close
-              }
-              aria-label="Open menu burger"
-            />
-          </div>
+        <button
+          id="button_navbar"
+          type="button"
+          onClick={toggleMenu}
+          className={
+            isMenuOpen === true
+              ? styles.burger_nav_open
+              
+              : styles.cross_nav_close
+          }
+          aria-label="Open menu burger"
+        />
+      </div>
 
-          {isMenuOpen === true && (
-          <section className={styles.handle_menu}>
-            <ul className={styles.navbar_links_container}>
-              <li className={styles.navbar_links_items}>
-                <a href={styles.navbar_route}>Our Librairie</a>
-              </li>
-              <li className={styles.navbar_links_items}>
-                <a href={styles.navbar_route}>Our Teams</a>
-              </li>
-              <li className={styles.navbar_links_items}>
-                <a href={styles.navbar_route}>My basket</a>
-              </li>
-              <li className={styles.navbar_links_items}>
-                <a href={styles.navbar_route}>Contact</a>
-              </li>
-            </ul>
-          </section>
-        )}
-    </nav>  
+      {isMenuOpen === true && (
+        <section className={styles.handle_menu}>
+          <ul className={styles.navbar_links_container}>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>Our Librairie</a>
+            </li>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>Our Teams</a>
+            </li>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>My basket</a>
+            </li>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>Contact</a>
+            </li>
+          </ul>
+        </section>
+      )}
+    </nav>
   );
 }
 
