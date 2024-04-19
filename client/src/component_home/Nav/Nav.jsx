8a8cee2c76fbdaef513aedf1e04cf435bd88c66b
Loic legoff <loic95280@gmail.com>
2024-04-19 16:32:26 +0200

    add navBar

--- client/src/component_home/Nav/Nav.jsx
+++ client/src/component_home/Nav/Nav.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
 import styles from "./Nav.module.css";
 
 function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.navbarLogo}>Logo</div>
-      <ul className={styles.navbarLinks}>
-        <li className={styles.navbarLinksItems}>
-          <a href={styles.navbarRoute}>Our Team</a>
-        </li>
-        <li className={styles.navbarLinksItems}>
-          <a href={styles.navbarRoute}>Our Librairie</a>
-        </li>
-        <li className={styles.navbarLinksItems}>
-          <a href={styles.navbarRoute}>My basket</a>
-        </li>
-        <li className={styles.navbarLinksItems}>
-          <a href={styles.navbarRoute}>Contact</a>
-        </li>
-      </ul>
+      <div className={styles.nav_container}>
+        <div className={styles.logo_Cover4You} />
+
+        <button
+          id="button_navbar"
+          type="button"
+          onClick={toggleMenu}
+          className={
+            isMenuOpen === true
+              ? styles.burger_nav_open
+              : styles.cross_nav_close
+          }
+          aria-label="Open menu burger"
+        />
+      </div>
+
+      {isMenuOpen === true && (
+        <section className={styles.handle_menu}>
+          <ul className={styles.navbar_links_container}>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>Our Librairie</a>
+            </li>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>Our Books</a>
+            </li>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>My basket</a>
+            </li>
+            <li className={styles.navbar_links_items}>
+              <a href={styles.navbar_route}>Contact</a>
+            </li>
+          </ul>
+        </section>
+      )}
     </nav>
   );
 }
