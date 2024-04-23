598116500d4343113c7f562d82d9469358e1aade
fadili6 <diallofadili@yahoo.fr>
2024-04-23 17:29:49 +0200

    personnalisations

--- client/src/component_home/BookStyle/BookStyle.jsx
+++ client/src/component_home/BookStyle/BookStyle.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./BookStyle.module.css";
 
+
 function BookStyle() {
   const [images, setImages] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
