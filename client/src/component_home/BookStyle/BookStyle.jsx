7b207825c67f7194010fbf98d7d81274360efe87
fadili6 <diallofadili@yahoo.fr>
2024-04-23 15:32:12 +0200

    ajout images API

--- client/src/component_home/BookStyle/BookStyle.jsx
+++ client/src/component_home/BookStyle/BookStyle.jsx
@@ -1,18 +1,58 @@
-import styles from "./BookStyle.module.css";
+import { useState, useEffect } from "react";
+import "./BookStyle.module.css";
 
 function BookStyle() {
+  const [images, setImages] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://openlibrary.org/search.json?q=ebook_access:[borrowable TO *] -key:"/works/OL38986W" author_key:(OL19441A)'
+        );
+        const data = await response.json();
+        // Extraction URLs des couvertures des neuf premiers résultats.
+        const covers =
+          data?.docs
+            ?.slice(0, 9)
+            .map((book) =>
+              book.cover_i
+                ? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
+                : ""
+            ) || [];
+        setImages(covers);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 3) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  const getNextImages = () => {
+    const nextImages = [];
+    for (let i = 0; i < 3; i += 1) {
+      nextImages.push(images[(currentIndex + i) % images.length]);
+    }
+    return nextImages;
+  };
+
   return (
-    <section className={styles.BookStyle}>
-      <div>
-        <img src="src/assets/fonts/pictures-books/book1.jpg" alt="livre1" />
-      </div>
-      <div>
-        <img src="src/assets/fonts/pictures-books/book2.jpg" alt="livre2" />
-      </div>
-      <div>
-        <img src="src/assets/fonts/pictures-books/book3.jpg" alt="livre3" />
-      </div>
-    </section>
+    <div>
+      <p>
+        <b>Our personalizations</b>
+      </p>
+      {getNextImages().map((image, index) => (
+        <img key={index.id} src={image} alt={`livre${currentIndex + index + 1}`} />
+      ))}
+    </div>
   );
 }
 
