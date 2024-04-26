f4d5408ef89b9e8d1346e276cfeef6fe4a9a33bf
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-04-26 10:06:24 +0200

    added a loading effect to Homepage and ResultPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 // CSS GLOBAL //
 import "./App.module.css";
+
 import { Outlet } from "react-router-dom";
+
 import Nav from "./component_home/Nav/Nav";
 import SearchBar from "./component_home/SearchBar/SearchBar";
 import Footer from "./component_home/Footer/Footer";
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,47 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import styles from "../ui_components/Loader.module.css";
 import BookList from "../component_home/BookList/BookList";
 
 export default function Home() {
-  const bookList = useLoaderData("home");
-  return <BookList bookList={bookList} />;
+  const [isLoading, setIsLoading] = useState(true);
+  const [bookList, setBookList] = useState([]);
+
+  const fetchBooks = async () => {
+    const homeBooks = [
+      "twilight",
+      "harry+potter",
+      "dragon+ball+z",
+      "l%27%C3%A9tranger",
+    ];
+    try {
+      const requests = homeBooks.map((book) =>
+        axios.get(`https://openlibrary.org/search.json?q=${book}`)
+      );
+
+      const responses = await Promise.all(requests);
+
+      const fetchedBooks = responses.map((response) => response.data.docs[0]);
+      setBookList(fetchedBooks);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+  return (
+    <>
+      {isLoading && (
+        <div className={styles.container}>
+          <div className={styles.loader} />
+        </div>
+      )}
+      <BookList bookList={bookList} />
+    </>
+  );
 }
